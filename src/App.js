ff68899b03e174c64c989f87963e010947cc1798
TahmimShovon <tahmim14@gmail.com>
2023-08-23 17:31:42 +0600

    Halfway reached

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { Footer, Header, Blog, Possibility, WhatGPT3, Features } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+import './index.css';
+
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import  './header.css'
 
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Beneath the shimmering stars, a world of dreams unfolds. Lost in the pages, her imagination knows no bounds. With each step, the journey becomes more uncertain yet exciting. Whispers of the wind carry secrets through the ancient trees.</p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people'/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai"/>
+        </div>
+    </div>
   )
 }
 
